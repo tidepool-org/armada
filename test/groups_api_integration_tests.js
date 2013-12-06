81193d70a692434a82cdc3af3e2320a20ae4b80f
Jamie Bate <jamie@tidepool.org>
2013-12-06 13:33:02 +1300

    build out ceate tests for valid group

--- test/groups_api_integration_tests.js
+++ test/groups_api_integration_tests.js
@@ -25,7 +25,7 @@ describe('message API', function() {
 
     describe('post /api/group/create', function() {
 
-        it('returns 400 when no group to create sent', function(done) {
+        it('returns 400 when given an invalid group to create', function(done) {
 
             var testGroup = {
                 nothing : ''
@@ -35,6 +35,24 @@ describe('message API', function() {
             .post('/api/group/create')
             .send({group:testGroup})
             .expect(400)
+            .end(function(err, res) {
+                if (err) return done(err);
+                done();
+            });
+        });
+
+        it('returns 201 when a group is created', function(done) {
+
+            var testGroup = {
+                name : 'integration test',
+                owners: ['99999','222222'],
+                patient : '12345'
+            };
+
+            supertest(apiEndPoint)
+            .post('/api/group/create')
+            .send({group:testGroup})
+            .expect(201)
             .end(function(err, res) {
                 if (err) return done(err);
                 done();

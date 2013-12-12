aec6a77bb77ba7754b6c83e16f1a9c32c7091641
Jamie Bate <jamie@tidepool.org>
2013-12-13 11:38:49 +1300

    correct should setup

--- test/fixtures.js
+++ test/fixtures.js
@@ -1,5 +1,5 @@
 var chai = require('chai');
 
-exports.should = chai.should;
+exports.should = chai.should();
 exports.supertest = require('supertest');
 exports.TestingHelper = require('./TestingHelper');

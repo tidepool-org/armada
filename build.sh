c1413183680ae7692cdf09b18809f11d662126b0
Jamie <jamie@tidepool.org>
2014-01-26 09:17:39 +1300

    requirements for mongo to be setup on server to run all tests

--- build.sh
+++ build.sh
@@ -1,3 +1,29 @@
 #! /bin/bash -eu
 
+# cleanup modules then re-install
+rm -rf node_modules
 npm install .
+
+BUILD_DIR=build
+
+# cleanup for mongo
+function cleanup {
+  echo "Killing pid[$1]"
+  kill -9 $1;
+  echo "Deleting the build directory: ${BUILD_DIR}"
+  rm -r ${BUILD_DIR};
+}
+
+# mongo setup, a requirement for the integration tests
+rm -rf ${BUILD_DIR}
+DBPATH=${BUILD_DIR}/mongo
+mkdir -p ${DBPATH}
+mongod --noprealloc --nojournal --smallfiles --dbpath ${DBPATH} --logpath ${BUILD_DIR}/mongo_log.log &
+MONGO_PID=$!
+echo "Sleeping to let mongo start up"
+sleep 3
+echo "Running tests"
+trap "cleanup ${MONGO_PID}" EXIT
+
+# run tests
+./node_modules/.bin/grunt test

5064b0ec62b30a97c38254e0b2e45637f11fe528
Jamie <jamie@tidepool.org>
2014-02-11 16:27:12 +1300

    jshint cleanup

--- test/mocks/mockUserApi.js
+++ test/mocks/mockUserApi.js
@@ -14,18 +14,20 @@ You should have received a copy of the License along with this program; if
 not, you can obtain one from Tidepool Project at tidepool.org.
 == BSD2 LICENSE ==
  */
- 
-var restify = require('restify'), 
-	sessiontoken = '99406ced-8052-49c5-97ee-547cc3347da6',
-	mockUserApi,
-	user;
 
-//fake user we will return 
+'use strict';
+
+var restify = require('restify'),
+  sessiontoken = '99406ced-8052-49c5-97ee-547cc3347da6',
+  mockUserApi,
+  user;
+
+//fake user we will return
 user = {
-	userid: 'a3d6a658-6e6a-401b-bcb3-c99268ba1804',
-  	username: 'realname',
-  	emails: ['foo@bar.com','myfoo@bar.org'],
-  	password: 'R6LvLQ$=aTBgfj&4jqAq'
+  userid: 'a3d6a658-6e6a-401b-bcb3-c99268ba1804',
+  username: 'realname',
+  emails: ['foo@bar.com','myfoo@bar.org'],
+  password: 'R6LvLQ$=aTBgfj&4jqAq'
 };
 
 //basic setup
@@ -35,44 +37,44 @@ mockUserApi.use(restify.bodyParser());
 
 //mock the end points we care about
 mockUserApi.post('/serverlogin', function(req, res, next) {
-	var server = req.headers['x-tidepool-server-name'];
-    var pw = req.headers['x-tidepool-server-secret'];
+  var server = req.headers['x-tidepool-server-name'];
+  var pw = req.headers['x-tidepool-server-secret'];
 
-    if((server && pw)){
-		res.header('x-tidepool-session-token', sessiontoken);
-		res.send(200,'machine login');
-		return next();    	
-    }
-
-    res.send(401, 'Server identity not validated!');
+  if((server && pw)){
+    res.header('x-tidepool-session-token', sessiontoken);
+    res.send(200,'machine login');
     return next();
-  
+  }
+
+  res.send(401, 'Server identity not validated!');
+  return next();
+
 });
 
 mockUserApi.get('/user/:id', function(req, res, next) {
-	var token = req.headers['x-tidepool-session-token'];
+  var token = req.headers['x-tidepool-session-token'];
 
-	if(token){
-		res.send(200,user);
-		return next();
-	}
+  if(token){
+    res.send(200,user);
+    return next();
+  }
 
-	res.send(401, 'Unauthorized');
-	return next();
-  
+  res.send(401, 'Unauthorized');
+  return next();
+
 });
 
 mockUserApi.get('/token/:usertoken', function(req, res, next) {
-	var token = req.params.usertoken;
+  var token = req.params.usertoken;
 
-	if(token){
-		res.send(200,{userid: user.userid});
-		return next();
-	}
+  if(token){
+    res.send(200,{userid: user.userid});
+    return next();
+  }
 
-	res.send(401, 'Unauthorized');
-	return next();
-  
+  res.send(401, 'Unauthorized');
+  return next();
+
 });
 
 module.exports = mockUserApi;
